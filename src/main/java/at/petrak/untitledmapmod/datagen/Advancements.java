5789b2a390fe364b8a0bf32abdd375694cc49a7d
gamma-delta <joshua.j.park@gmail.com>
2022-03-02 10:54:12 -0600

    make advancements display properly

--- src/main/java/at/petrak/untitledmapmod/datagen/Advancements.java
+++ src/main/java/at/petrak/untitledmapmod/datagen/Advancements.java
@@ -8,10 +8,13 @@ import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.advancements.FrameType;
 import net.minecraft.advancements.critereon.EntityPredicate;
+import net.minecraft.advancements.critereon.TickTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.advancements.AdvancementProvider;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,13 +31,23 @@ public class Advancements extends AdvancementProvider {
 
     @Override
     protected void registerAdvancements(Consumer<Advancement> consumer, ExistingFileHelper fileHelper) {
+        var root = Advancement.Builder.advancement()
+            .display(Items.COMPASS, new TranslatableComponent("advancement." + UntitledMapMod.MOD_ID + ":root"),
+                new TranslatableComponent("advancement." + UntitledMapMod.MOD_ID + ":root.desc"),
+                new ResourceLocation("minecraft:textures/block/cartography_table_side3.png"),
+                FrameType.TASK, true, true, false)
+            .addCriterion("i_cant_think_of_anything", new TickTrigger.TriggerInstance(EntityPredicate.Composite.ANY))
+            .save(consumer, prefix("root"));
+
         Advancement.Builder.advancement()
             .display(simple(ModItems.WORLD_MAP.get(), "world_map", FrameType.TASK))
+            .parent(root)
             .addCriterion("on_use", new UseMapUnlockerTrigger.Instance(EntityPredicate.Composite.ANY, false))
             .save(consumer, prefix("world_map"));
 
         Advancement.Builder.advancement()
             .display(simple(ModItems.MINIMAP.get(), "minimap", FrameType.TASK))
+            .parent(root)
             .addCriterion("on_use", new UseMapUnlockerTrigger.Instance(EntityPredicate.Composite.ANY, true))
             .save(consumer, prefix("minimap"));
     }
@@ -49,7 +62,7 @@ public class Advancements extends AdvancementProvider {
     }
 
     protected static DisplayInfo simple(ItemLike icon, String name, FrameType frameType) {
-        var expandedName = "advancement.untitledmapmod." + name;
+        var expandedName = "advancement." + UntitledMapMod.MOD_ID + ":" + name;
         return new DisplayInfo(new ItemStack(icon.asItem()),
             new TranslatableComponent(expandedName),
             new TranslatableComponent(expandedName + ".desc"),
